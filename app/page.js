9788f0c420a1ceae1b17ecccde7d7050ff51e635
kksimons <kksimons@gmail.com>
2024-05-14 12:16:50 -0600

    Removed styles
    Removed styles

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 import Link from "next/link";
 
 export default function Home() {
-  const linkStyles = "underline text-green-600 hover:text-green-300";
-
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
