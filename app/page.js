887b1d1ec020b2e37d415263b05c7fbb1ec7d2f6
kksimons <kksimons@gmail.com>
2024-05-14 12:19:03 -0600

    Really removed styles
    For real

--- app/page.js
+++ app/page.js
@@ -4,9 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link className={linkStyles} href="./week-2">
-        week-2
-      </Link>
+      <Link href="./week-2">week-2</Link>
     </main>
   );
 }
