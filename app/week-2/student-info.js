dbb2895c4f374da157fa499ce9c43c444e093f34
kksimons <kksimons@gmail.com>
2024-05-14 12:11:54 -0600

    Initial Commit w/page.js and student-info
    Just need to tweak tailwind styles

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  const studentName = "Kyle Simons";
+  const githubUrl = "https://github.com/kksimons";
+  const linkStyles = "underline text-green-600 hover:text-green-300";
+
+  return (
+    <div className="bg-color: black">
+      <h2>{studentName}</h2>
+      <Link className={linkStyles} href={githubUrl}>
+        My Github
+      </Link>
+    </div>
+  );
+}
