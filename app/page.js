db88fcd9dfa884e7a540eb4a7860fdc06bedc93a
kksimons <kksimons@gmail.com>
2024-05-21 12:28:41 -0600

    Update styling for main page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24 space-y-2">
+    <main className="flex min-h-screen flex-col items-center p-24 space-y-2">
       <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
       <p className="hover:text-red-700 hover:underline">
         <Link href="./week-2">Week 2 Assignment</Link>
