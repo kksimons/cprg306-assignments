ca38aa9f2ab7a9db3234318e0a63e7a38fe181f0
kksimons <kksimons@gmail.com>
2024-05-21 12:14:17 -0600

    Tailwind styling so bg color doesn't span entire width

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,10 +2,10 @@ export default function Item({ item }) {
   const { name, quantity, category } = item;
 
   return (
-    <div className="bg-gray-100 p-4 m-2">
+    <div className="bg-gray-100 p-4 m-2 rounded w-fit">
       <li>
         <ul className="font-bold text-lg">{name}</ul>
-        <ul className="text-md">
+        <ul className="text-sm">
           Buy {quantity} in {category}
         </ul>
       </li>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list.js";
 export default function Page() {
   return (
     <main>
-      <h1 className="text-xl font-bold">Shopping List</h1>
+      <h1 className="text-3xl font-bold">Shopping List</h1>
       <ItemList />
     </main>
   );
